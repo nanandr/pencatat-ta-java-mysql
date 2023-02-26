9f98e9a1d15099d814513c40a8d63618edefa2d0
nanandr <rafirenan12@gmail.com>
2023-02-26 07:15:25 +0700

    add activity

--- app/src/main/java/com/nandanarafiardika/pencatatbukuta/AddActivity.java
+++ app/src/main/java/com/nandanarafiardika/pencatatbukuta/AddActivity.java
@@ -2,7 +2,9 @@ package com.nandanarafiardika.pencatatbukuta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
 import android.os.AsyncTask;
@@ -13,25 +15,57 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class AddActivity extends AppCompatActivity {
-    private EditText editTextNoInduk, editTextJudul, editTextNamaPemilik, editTextNamaPembimbing, editTextTempatPkl, editTextAngkatan, editTextKelas;
-    Button buttonAdd;
+    private EditText editTextNoInduk, editTextJudul, editTextNamaPemilik, editTextNamaPembimbing, editTextTempatPkl, editTextKelas;
+    Button buttonAngkatan, buttonAdd;
     ImageView buttonBack;
+    int currentAngkatan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        Calendar calendar = Calendar.getInstance();
+        Date date = new Date();
+        calendar.setTime(date);
+
+        int currentMonth = calendar.get(Calendar.MONTH);
+        int currentYear = calendar.get(calendar.YEAR);
+
         editTextNoInduk = findViewById(R.id.no_induk);
         editTextJudul = findViewById(R.id.judul);
         editTextNamaPemilik = findViewById(R.id.nama_pemilik);
         editTextNamaPembimbing = findViewById(R.id.nama_pembimbing);
-        editTextAngkatan = findViewById(R.id.angkatan);
         editTextKelas = findViewById(R.id.kelas);
         editTextTempatPkl = findViewById(R.id.tempat_pkl);
+
+        if(currentMonth < 6){
+            currentAngkatan = currentYear - 2003;
+        }
+        else{
+            currentAngkatan = currentYear - 2002;
+        }
+
+        buttonAngkatan = findViewById(R.id.angkatan);
+        buttonAngkatan.setText(String.valueOf(currentAngkatan-2));
+        buttonAngkatan.setOnClickListener(view -> {
+            String[] options = new String[currentAngkatan + 1];
+            for (int i = 1; i <= currentAngkatan; ++i) {
+                options[i-1] = Integer.toString(i);
+            }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(AddActivity.this);
+            builder.setTitle("Pilih Angkatan");
+            builder.setItems(options, (dialogInterface, which) -> buttonAngkatan.setText(options[which]));
+            builder.show();
+        });
 
         buttonAdd = findViewById(R.id.add);
         buttonBack = findViewById(R.id.back);
@@ -41,7 +75,7 @@ public class AddActivity extends AppCompatActivity {
             final String judul = editTextJudul.getText().toString();
             final String namaPemilik = editTextNamaPemilik.getText().toString();
             final String namaPembimbing = editTextNamaPembimbing.getText().toString();
-            final String angkatan = editTextAngkatan.getText().toString().trim();
+            final String angkatan = buttonAngkatan.getText().toString().trim();
             final String kelas = editTextKelas.getText().toString();
             final String tempatPkl = editTextTempatPkl.getText().toString();
 
